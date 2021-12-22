f2d5672da6246897b16634c4f55aada5ac5a6105
jmulato <mulatojanrick@gmail.com>
2021-12-22 11:31:40 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="home">
+    <DirectoryTreeView></DirectoryTreeView>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import DirectoryTreeView from "@/components/TreeView/DirectoryTreeView/DirectoryTreeView.vue"; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
+    DirectoryTreeView,
   },
 })
-export default class App extends Vue {}
+export default class Home extends Vue {}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
